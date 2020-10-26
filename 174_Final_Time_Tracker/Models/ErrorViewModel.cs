cae8dd86f2aa0aa4798eb990ea09a1505e36ddb9
jsharpe13 <58717569+jsharpe13@users.noreply.github.com>
2020-10-26 00:53:09 -0500

    Add project files.

---
+++ 174_Final_Time_Tracker/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _174_Final_Time_Tracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
